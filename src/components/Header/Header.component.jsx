c2d2669a1536297226510bef104ecadae44273e5
nguyendinhphuc <phucdinhnguyen09052000@gmail.com>
2019-09-25 06:43:29 -0700

    demo

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 
 const Header = () => (
-  <header className="header-section">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-3 col-md-3">
-          <div className="site-logo">
-            <img src="img/logo.png" alt />
-          </div>
-          <div className="nav-switch">
-            <i className="fa fa-bars" />
-          </div>
+ <header className="header-section">
+  <div className="container">
+    <div className="row">
+      <div className="col-lg-3 col-md-3">
+        <div className="site-logo">
+          <img src="img/logo.png" alt />
         </div>
-        <div className="col-lg-9 col-md-9">
-          <a href className="site-btn header-btn">Login</a>
-          <nav className="main-menu">
-            <ul>
-              <li><a href="index.html">Home</a></li>
-              <li><a href="#">About us</a></li>
-              <li><a href="courses.html">Courses</a></li>
-              <li><a href="blog.html">News</a></li>
-              <li><a href="contact.html">Contact</a></li>
-            </ul>
-          </nav>
+        <div className="nav-switch">
+          <i className="fa fa-bars" />
         </div>
       </div>
+      <div className="col-lg-9 col-md-9">
+        <a href className="site-btn header-btn">Login</a>
+        <nav className="main-menu">
+          <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="courses.html">Courses</a></li>
+            <li><a href="blog.html">News</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
     </div>
-  </header>
+  </div>
+</header>
 
 );
 export default Header;
